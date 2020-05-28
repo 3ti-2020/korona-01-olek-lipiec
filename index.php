73089f8e6d67ff6302bce4d891246b7276600a2f
Fanfaryy <34983357+Fanfaryy@users.noreply.github.com>
2020-05-28 11:07:57 +0200

    Relacja wiele do wielu - add.php, table.php

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 
-    include("header.php");
+    <?php
 
-    echo("<div class='container'>".file_get_contents('http://loripsum.net/api')."</div>");
+        require("table.php");
 
-    include("footer.php");
-?>
+        table("SELECT * FROM autorzy, tytuly, krzyzowa WHERE krzyzowa.id_autor=autorzy.id_autor AND krzyzowa.id_tytul=tytuly.id_tytul");
+
+    ?>
+
+    <form action="add.php" method="GET">
+
+        <input type="text" name="imie-autora">
+        <input type="text" name="nazwisko-autora">
+        <input type="text" name="tytul">
+        <input type="text" name="isbn">
+        <input type="submit" value="Dodaj">
+
+    </form>
+
+    <form action="remove.php" method="GET">
+
+        <input type="text" name="imie-autora">
+        <input type="text" name="nazwisko-autora">
+        <input type="text" name="tytul">
+        <input type="submit" value="Usuń">
+
+    </form>
+    
+</body>
+</html>
